2535b16c72ec9e431d5823d28a798349e0a6826a
Edy Galdiolo <edygaldy302@gmail.com>
2023-07-12 16:55:56 +0200

    feat: made admin moved dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,11 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])
+	->prefix('admin')
+	->name('admin.')
+	->group(function () {
+	
+		Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+});
 
 require __DIR__.'/auth.php';
